ea740d4fe32cf0db44e1e0b4a765786751de13c8
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-06-09 03:28:02 -0500

    Added summaries to the abstract activation function and parameters classes

--- NeuralNetworkFundamentals/Activation Functions/ActivationFunction.cs
+++ NeuralNetworkFundamentals/Activation Functions/ActivationFunction.cs
@@ -1,13 +1,32 @@
 ﻿namespace NeuralNetworkFundamentals.Activation_Functions
 {
+    /// <summary>
+    /// Class for implementing parameters to be passed into activation functions, if need be.
+    /// </summary>
     public class ActivationParameters { };
+
+    /// <summary>
+    /// Class for implementing activation functions from
+    /// </summary>
     public abstract class ActivationFunction
     {
         // For information on how to figure out the standard activation function algorithms
         // Go here: https://en.wikipedia.org/wiki/Activation_function
 
+        /// <summary>
+        /// Activates the function
+        /// </summary>
+        /// <param name="x">input value to use</param>
+        /// <param name="Params">parameters to use</param>
+        /// <returns></returns>
         public abstract double Activate(double x, ActivationParameters Params);
 
+        /// <summary>
+        /// Returns the derivative of the function for a given value
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="Params"></param>
+        /// <returns></returns>
         public abstract double Derivate(double x, ActivationParameters Params);
     }
 }
